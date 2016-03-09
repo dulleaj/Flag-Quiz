08d5fa8b027f08456a5260cddd4074edc3730130
Drew Dulle <dulleaj@gmail.com>
2016-03-08 19:16:30 -0500

    Added new class - Flag

---
+++ Emoji/Flag.h
@@ -0,0 +1,13 @@
+//
+//  Flag.h
+//  Emoji
+//
+//  Created by Andrew Dulle on 3/8/16.
+//  Copyright © 2016 Andrew Dulle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Flag : NSObject
+
+@end
---
+++ Emoji/Flag.m
@@ -0,0 +1,13 @@
+//
+//  Flag.m
+//  Emoji
+//
+//  Created by Andrew Dulle on 3/8/16.
+//  Copyright © 2016 Andrew Dulle. All rights reserved.
+//
+
+#import "Flag.h"
+
+@implementation Flag
+
+@end
